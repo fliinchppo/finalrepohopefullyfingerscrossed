aa1493c4058b44d2bdfc0aa1efd5410f894f407e
TarrielDRC <tarriel345@gmail.com>
2015-12-18 11:58:14 +1000

    Add JSON read support and basic weapon switching
    Adds the SimpleJSON library for reading JSON files and a file containing
    a list of available weapons. Some changes were made to WeaponSwap.cs to
    allow weapon switching in-game, but it is not yet functional.
    
    Mainly just committing my changes before I merge with the updated
    repository.
    
    Signed-off-by: TarrielDRC <tarriel345@gmail.com>

--- Assets/GameAssets/Scripts/WeaponSwap.cs
+++ Assets/GameAssets/Scripts/WeaponSwap.cs
@@ -1,13 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SimpleJSON;
 
 public class WeaponSwap : MonoBehaviour {
 	//-------Declare variables--------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	public string loadoutWeapon1;
+	public string loadoutWeapon2;
+	public JSONNode weapon1;
+	public JSONNode weapon2;
 	public int wSlot;
+
+	JSONArray weaponList;
+	TextAsset jsonList;
 
 	//-------Use this for initialization----------------------------------------------------------------------------------------------------------------------------------------
 	void Start () {
-
+		jsonList = Resources.Load ("weaponsList") as TextAsset;
+		var weaponList = JSON.Parse (jsonList.text);
+		weapon1 = weaponList [loadoutWeapon1];
+		weapon2 = weaponList [loadoutWeapon2];
 	}
 	
 	//-------Update is called once per frame------------------------------------------------------------------------------------------------------------------------------------
@@ -20,17 +32,28 @@ public class WeaponSwap : MonoBehaviour {
 		if (Input.GetButton ("SwapWeapon")) {
 			if (wSlot == 1) {
 				// swap to weapon slot 2 in array
+				wSlot = 2;
 			} else if (wSlot == 2) {
 				// swap to weapon slot 1 in array
+				wSlot = 1;
 			}
+			SetWeapon(wSlot);
 		}
 		
 		if (mwheelInput > 0f) {
 			// scroll up, swap to weapon slot 1 in array
+			wSlot = 1;
+			SetWeapon(wSlot);
 		}
 		
 		if (mwheelInput < 0f) {
 			// scroll down, swap to weapon slot 2 in array
+			wSlot = 2;
+			SetWeapon(wSlot);
 		}
+	}
+
+	void SetWeapon(int w) {
+		var  = GetComponent<AudioSource>();
 	}
 }
