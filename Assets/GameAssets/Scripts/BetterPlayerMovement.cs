4b8c2e8d3ef6c6bc179230a05759ba485f8fe327
fliinchppo <gfxelixir@gmail.com>
2015-12-19 17:18:45 +1000

    Better movement update
    ++ added player movement while in air
    ++ fixed player clipping issue (camera and walls)
    ++ tagged base models as map objects

--- Assets/GameAssets/Scripts/BetterPlayerMovement.cs
+++ Assets/GameAssets/Scripts/BetterPlayerMovement.cs
@@ -13,6 +13,8 @@ public class BetterPlayerMovement : MonoBehaviour {
 
 	//-------Declare variables--------------------------------------------------------------------------------------------------------------------------------------------------
 	public float speed;
+	public float jumpSpeed;
+	private float verticalVelocity = 0;
 	Vector3 direction = Vector3.zero;
 	CharacterController cc;
 	Animator anim;
@@ -25,18 +27,35 @@ public class BetterPlayerMovement : MonoBehaviour {
 	
 	//-------Update is called once per frame------------------------------------------------------------------------------------------------------------------------------------
 	void Update () {
-		direction = transform.rotation * new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")).normalized;
+		direction = transform.rotation * new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
 
-		if (direction.magnitude > 1f) {
-			direction = direction.normalized;
-		}
+		//if (direction.magnitude > 1f) {
+		//	direction = direction.normalized;
+		//}
 
 		anim.SetFloat ("Vspeed", Input.GetAxis ("Vertical"));
 		anim.SetFloat ("Hspeed", Input.GetAxis ("Horizontal"));
+
+		if (cc.isGrounded && Input.GetButtonDown("Jump")) {
+			verticalVelocity = jumpSpeed;
+		}
 	}
 
 	//-------Updated at a constant rate-----------------------------------------------------------------------------------------------------------------------------------------
 	void FixedUpdate() {
-		cc.SimpleMove (direction * speed);
+		Vector3 dist = direction * speed * Time.deltaTime;
+
+		if (cc.isGrounded && verticalVelocity < 0) {
+			//anim.SetBool ("Jumping", false);
+		} else {
+			if (Mathf.Abs (verticalVelocity) > jumpSpeed * 0.75f) {					
+				//anim.SetBool ("Jumping", true);
+			}
+
+			verticalVelocity += Physics.gravity.y * Time.deltaTime * 3;
+		}
+
+		dist.y = verticalVelocity * Time.deltaTime;
+		cc.Move (dist);
 	}
 }
